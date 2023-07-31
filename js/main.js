4dc850c71de49db37d6d666d5e19faa765597be4
Mansur017 <mansurtovs017@icloud.com>
2023-07-31 18:45:34 +0300

    function

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
 //   name = 3;
 // console.log(number, name);
 
-// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // const a = "28.02.1999"; // string
 // const b = 3.14; // number
@@ -42,39 +42,91 @@
 // // console.log(cash);
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const a = 15;
-if (a < 10) {
-  console.log("константа меньше 10");
-} else {
-  console.log("константа не меньше 10");
-}
+// const a = 15;
+// if (a < 10) {
+//   console.log("константа меньше 10");
+// } else {
+//   console.log("константа не меньше 10");
+// }
 
-let x = 10;
-y = 7;
-let c = x > y ? "x больше, чем y" : "x не больше, чем y";
-console.log(c);
+// let x = 10;
+// y = 7;
+// let c = x > y ? "x больше, чем y" : "x не больше, чем y";
+// console.log(c);
 
-const num = prompt("Введите число");
-if (num % 2 === 0) {
-  console.log(`Число ${num} четное`);
-} else {
-  console.log(`Число ${num} нечетное`);
-}
+// const num = prompt("Введите число");
+// if (num % 2 === 0) {
+//   console.log(`Число ${num} четное`);
+// } else {
+//   console.log(`Число ${num} нечетное`);
+// }
 
-let num1 = prompt("number1?");
-let num2 = prompt("number2?");
-let num3 = prompt("number3?");
+// let num1 = prompt("number1?");
+// let num2 = prompt("number2?");
+// let num3 = prompt("number3?");
 
-if (num1 > num2) {
-  if (num1 > num3) {
-    console.log(`${num1} больше всех`);
+// // if (num1 > num2) {
+// //   if (num1 > num3) {
+// //     console.log(`${num1} больше всех`);
+// //   } else {
+// //     console.log(`${num3} больше всех`);
+// //   }
+// // } else {
+// //   if (num2 > num3) {
+// //     console.log(`${num2} больше всех`);
+// //   } else {
+// //     console.log(`${num3} больше всех`);
+// //   }
+// }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function evenOrOdd(chifre) {
+  if (chifre % 2 === 0) {
+    console.log(`even`);
   } else {
-    console.log(`${num3} больше всех`);
-  }
-} else {
-  if (num2 > num3) {
-    console.log(`${num2} больше всех`);
-  } else {
-    console.log(`${num3} больше всех`);
+    console.log(`odd`);
   }
 }
+evenOrOdd(123);
+evenOrOdd(57);
+evenOrOdd(98);
+
+function isMajor(number) {
+  if (number > 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isMajor(28));
+
+function isLowerThanTen(number) {
+  if (number < 10) {
+    console.log("константа меньше 10");
+  } else {
+    console.log("константа не меньше 10");
+  }
+}
+isLowerThanTen(50);
+
+function showAlert() {
+  alert("Привет, Мир");
+}
+showAlert();
+
+function showconsole() {
+  console.log("Javascript");
+}
+showconsole();
+
+let text = "freedom";
+function showText(text) {
+  if (text === "lorem") {
+    console.log("У вас ошибка в тексте");
+  } else {
+    console.log(`Содержимое переменной: ${text}`);
+  }
+}
+showText(text);
